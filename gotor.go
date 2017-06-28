10280f9b187b5ff8a6d4ca0e083fcb4451bf5cd8
FrozenKP <csjh21010@gmail.com>
2017-06-28 12:29:36 +0800

    move eaxmple.go to eaxmple/

--- gotor.go
+++ gotor.go
@@ -4,12 +4,12 @@ code original from http://www.devdungeon.com/content/making-tor-http-requests-go
 package gotor
 
 import (
-  "log"
-  "net/http"
-  "net/url"
-  "time"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
 
-  "golang.org/x/net/proxy"
+	"golang.org/x/net/proxy"
 )
 
 // Specify Tor proxy ip and port
@@ -19,28 +19,28 @@ var torProxy string = "socks5://127.0.0.1:9050" // 9150 w/ Tor Browser
 //
 // Using "socks5://127.0.0.1:9050" as proxy in default, you can put your own proxy
 // in paramemter to replace it.
-func New(timeOut time.Duration, InputProxy ...string) *http.Client{
-  // Parse Tor proxy URL string to a URL type
-  var proxyURL string
-  if len(InputProxy) != 0 {
-    proxyURL = InputProxy[0]
-  }else{
-    proxyURL = torProxy
-  }
-  torProxyUrl, err := url.Parse(proxyURL)
-  if err != nil {
-    log.Fatal("Error parsing Tor proxy URL:", torProxy, ".", err)
-  }
+func New(timeOut time.Duration, InputProxy ...string) *http.Client {
+	// Parse Tor proxy URL string to a URL type
+	var proxyURL string
+	if len(InputProxy) != 0 {
+		proxyURL = InputProxy[0]
+	} else {
+		proxyURL = torProxy
+	}
+	torProxyUrl, err := url.Parse(proxyURL)
+	if err != nil {
+		log.Fatal("Error parsing Tor proxy URL:", torProxy, ".", err)
+	}
 
-  // Create proxy dialer using Tor SOCKS proxy
-  torDialer, err := proxy.FromURL(torProxyUrl, proxy.Direct)
-  if err != nil {
-    log.Fatal("Error setting Tor proxy.", err)
-  }
+	// Create proxy dialer using Tor SOCKS proxy
+	torDialer, err := proxy.FromURL(torProxyUrl, proxy.Direct)
+	if err != nil {
+		log.Fatal("Error setting Tor proxy.", err)
+	}
 
-  // Set up a custom HTTP transport to use the proxy and create the client
-  torTransport := &http.Transport{Dial: torDialer.Dial}
-  client := &http.Client{Transport: torTransport, Timeout: timeOut}
+	// Set up a custom HTTP transport to use the proxy and create the client
+	torTransport := &http.Transport{Dial: torDialer.Dial}
+	client := &http.Client{Transport: torTransport, Timeout: timeOut}
 
-  return client
+	return client
 }
